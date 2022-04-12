71aca4bb66ae3c466546e5c112d3c5e53826c348
aborensved <92864523+aborensved@users.noreply.github.com>
2022-04-12 17:11:53 +0200

    button fixes

--- src/main/java/com/example/advgdelv3/views/AppView.java
+++ src/main/java/com/example/advgdelv3/views/AppView.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
@@ -27,10 +28,11 @@ public class AppView extends AppLayout {
         navbarLayout.add(new DrawerToggle(),new H1("AD Game Reviews"));
 
 
-        Button loginButton = new Button("Login", evt ->
-                UI.getCurrent().navigate(LoginView.class));
+        Button loginButton = new Button("Login", evt -> UI.getCurrent().navigate(LoginView.class));
+        loginButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_CONTRAST);
 
         Button logoutButton = new Button("Logout", evt -> PrincipalUtil.logout());
+        logoutButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_CONTRAST);
 
         if(PrincipalUtil.isLoggedIn()){
             navbarLayout.add(logoutButton);
